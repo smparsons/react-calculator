8da74d8cbd5c90b3be990ddeec5daad09767dfc1
Samuel Parsons <sparsons@tradovate.com>
2019-04-07 19:12:48 -0400

    Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const DESTINATION = path.resolve(__dirname, 'dist')
+
+module.exports = {
+  entry: ['./src/index.tsx'],
+  output: {
+    filename: 'bundle.js',
+    path: DESTINATION
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    },
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    modules: ['./src', 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'source-map-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'tslint-loader'
+      },
+      {
+        test: /\.tsx$/,
+        exclude: [/node_modules/],
+        use: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(), 
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    publicPath: '/',
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true
+  }
+}
