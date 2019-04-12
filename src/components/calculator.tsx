a15465790f81712ade91501af9ba84cf91c7e010
Samuel Parsons <samuel.m.parsons@gmail.com>
2019-04-12 06:54:07 -0400

    More css fixes

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -3,34 +3,36 @@ import * as React from 'react'
 export const Calculator = () => (
   <div className="calculator-wrapper">
     <div className="calculator-display">0</div>
-    <div className="calculator-button-row">
-      <button className="calculator-button helper">AC</button>
-      <button className="calculator-button helper">+/-</button>
-      <button className="calculator-button helper">%</button>
-      <button className="calculator-button operator">/</button>
-    </div>
-    <div className="calculator-button-row">
-      <button className="calculator-button number">7</button>
-      <button className="calculator-button number">8</button>
-      <button className="calculator-button number">9</button>
-      <button className="calculator-button operator">x</button>
-    </div>
-    <div className="calculator-button-row">
-      <button className="calculator-button number">4</button>
-      <button className="calculator-button number">5</button>
-      <button className="calculator-button number">6</button>
-      <button className="calculator-button operator">-</button>
-    </div>
-    <div className="calculator-button-row">
-      <button className="calculator-button number">1</button>
-      <button className="calculator-button number">2</button>
-      <button className="calculator-button number">3</button>
-      <button className="calculator-button operator">+</button>
-    </div>
-    <div className="calculator-button-bottom-row">
-      <button className="calculator-button number">0</button>
-      <button className="calculator-button number">.</button>
-      <button className="calculator-button operator">=</button>
+    <div className="calculator-buttons">
+      <div className="calculator-button-row">
+        <button className="calculator-button helper">AC</button>
+        <button className="calculator-button helper">+/-</button>
+        <button className="calculator-button helper">%</button>
+        <button className="calculator-button operator">/</button>
+      </div>
+      <div className="calculator-button-row">
+        <button className="calculator-button number">7</button>
+        <button className="calculator-button number">8</button>
+        <button className="calculator-button number">9</button>
+        <button className="calculator-button operator">x</button>
+      </div>
+      <div className="calculator-button-row">
+        <button className="calculator-button number">4</button>
+        <button className="calculator-button number">5</button>
+        <button className="calculator-button number">6</button>
+        <button className="calculator-button operator">-</button>
+      </div>
+      <div className="calculator-button-row">
+        <button className="calculator-button number">1</button>
+        <button className="calculator-button number">2</button>
+        <button className="calculator-button number">3</button>
+        <button className="calculator-button operator">+</button>
+      </div>
+      <div className="calculator-button-row">
+        <button className="calculator-button number zero">0</button>
+        <button className="calculator-button number">.</button>
+        <button className="calculator-button operator">=</button>
+      </div>
     </div>
   </div>
 )
